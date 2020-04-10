e6911871947e828f65b251178bdc3daf81f1d744
larsnoya <larsnoya@gmail.com>
2020-04-10 13:56:16 +0800

    feat : 實作球與多個實體碰撞偵測

--- app/src/main/java/com/example/edgecollisiondetect/MainActivity.kt
+++ app/src/main/java/com/example/edgecollisiondetect/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.edgecollisiondetect
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
@@ -13,10 +15,25 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     var lastX: Int = 0
     var lastY: Int = 0    //保存手指点下的点的坐标
+    lateinit var ballList: MutableList<Ball>
+    lateinit var ballHandler : Handler
+    var mThread: HandlerThread = HandlerThread("BallThread")
+    lateinit var viewList:MutableList<View>
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewList = mutableListOf<View>()
+        viewList.add(button)
+        viewList.add(imageView)
+        mThread.start()
+        ballHandler = Handler(mThread.getLooper())
+        ballList = mutableListOf<Ball>()
+        var mBall = Ball(this@MainActivity, ballHandler,viewList)
+        ballList.add(mBall)
+        for (i in 0 until ballList.size) {
+            ballList[i].start()
+        }
         //初始设置一个layoutParams
         val layoutParams = RelativeLayout.LayoutParams(100, 100)
         imageView!!.layoutParams = layoutParams
@@ -49,37 +66,48 @@ class MainActivity : AppCompatActivity() {
                 }
                 //刷新界面
                 view.invalidate()
-                isTouchButonEdge(view)
+//                ItemEdgeDetect(view)
                 return true
             }
         })
     }
-    fun isTouchButonEdge(mView:View) :Boolean{
-        var mButton = button
+    fun isTouchButonEdge(mView:View,BeHitObject : View) :Boolean{
+        var mBeHitObject = BeHitObject
         //左右判斷
-        if(mView.bottom <= button.bottom+mView.height /2 && mView.top >=button.top-mView.height /2) {
+        if(mView.bottom <= mBeHitObject.bottom+mView.height /2 && mView.top >=mBeHitObject.top-mView.height /2) {
             //right
-            if (mView.right == button.left && mView.left < button.left) {
+            if (mView.right == mBeHitObject.left && mView.left < mBeHitObject.left) {
                 Log.d("touch", "right Touch!")
             }
             //left
-            if (mView.left == button.right && mView.right > button.right) {
+            if (mView.left == mBeHitObject.right && mView.right > mBeHitObject.right) {
                 Log.d("touch", "left Touch!")
             }
         }
 //        上下判斷
-        if ((mView.left >= button.left - mView.width /2) &&
-            (mView.right  <= button.right +mView.width /2)
+        if ((mView.left >= mBeHitObject.left - mView.width /2) &&
+            (mView.right  <= mBeHitObject.right +mView.width /2)
         ){
             //bottom
-            if (mView.top < button.bottom && mView.bottom == button.top) {
+            if (mView.top < mBeHitObject.bottom && mView.bottom == mBeHitObject.top) {
                 Log.d("touch", "bottom Touch!")
             }
             //top
-            if (mView.top == button.bottom && mView.bottom > button.bottom) {
+            if (mView.top == mBeHitObject.bottom && mView.bottom > mBeHitObject.bottom) {
                 Log.d("touch", "top Touch!")
             }
         }
         return true
+    }
+    fun killBalls(mBall: Ball) {
+        var index = ballList.indexOfFirst { it == mBall }
+        var relative = findViewById<RelativeLayout>(R.id.activity_main_relativelayout)
+        relative!!.removeView(ballList[index].imageView)
+        ballList.remove(mBall)
+//        showBallCount()
+    }
+    companion object {
+        @kotlin.jvm.JvmField
+        var MOVE_IMAGE: Int = 1
     }
 }
